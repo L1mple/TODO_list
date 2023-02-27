310a125965f3ca0a4ec026cbc47d57e277705232
L1mple <L1mple@rambler.ru>
2023-02-27 11:29:57 +0300

    Slightly better way to setup celery worker

--- todo/service/celery/celery_worker.py
+++ todo/service/celery/celery_worker.py
@@ -1,14 +1,32 @@
 from celery import Celery
+from dependency_injector.wiring import Provide, inject
+
+from todo.dependencies import Container
+from todo.service.celery.settings import CelerySettings
 
 
-def create_celery(app_name: str, broker_url: str, backend_url: str) -> Celery:
+def create_celery_app() -> Celery:
     """Factory for celery app."""
-    celery_app = Celery(app_name, broker=broker_url, backend=backend_url)
+    container = Container()
+    container.wire(modules=[__name__])
+    celery_app = Celery(
+        container.celery_settings.provided.NAME,
+        broker=container.celery_settings.provided.broker_connection_str(),
+        backend=container.celery_settings.provided.result_connection_str(),
+    )
     return celery_app
 
 
-celeryyy = create_celery(
-    "tasks",
-    broker_url="pyamqp://admin:pa55w0rd@rabbitmq:5672/",
-    backend_url="pyamqp://admin:pa55w0rd@rabbitmq:5672/",
-)
+celeryyy = create_celery_app()
+
+
+# @inject
+# def create_celery(celery_settings: CelerySettings = Provide[Container.celery_settings]) -> Celery:
+#     """Factory for celery app."""
+#     celery_app = Celery(
+#         celery_settings.NAME,
+#         broker=celery_settings.broker_connection_str(),
+#         backend=celery_settings.result_connection_str(),
+#     )
+#     return celery_app
+# Еще я пытался сделать так, но там 'Provide' object has no attribute NAME ошибка была и я не смог нагуглить как пофиксить(
--- todo/service/celery/settings.py
+++ todo/service/celery/settings.py
@@ -5,11 +5,14 @@ class CelerySettings(BaseSettings):
     """Settings for MongoDB connection."""
 
     NAME: str = Field(default="tasks")
-    HOST: str = Field(default="rabbitmq")
-    PORT: int = Field(default=5672)
-    USERNAME: str = Field(default="admin")
-    PASSWORD: SecretStr = Field(default="pa55w0rd")
-    DATABASE: str = Field(default="todo")
+    BROKER_HOST: str = Field(default="rabbitmq")
+    BROKER_PORT: int = Field(default=5672)
+    BROKER_USERNAME: str = Field(default="admin")
+    BROKER_PASSWORD: SecretStr = Field(default="pa55w0rd")
+    BROKER_DATABASE: str = Field(default="todo")
+    RESULT_HOST: str = Field(default="redis")
+    RESULT_PORT: int = Field(default=6379)
+    RESULT_PASSWORD: SecretStr = Field(default="pa55w0rd")
 
     class Config:
         """Settings for env files."""
@@ -17,10 +20,22 @@ class CelerySettings(BaseSettings):
         env_prefix = "CELERY_"
         frozen = True
 
-    def connection_str(self) -> str:
-        """Constructor for mongodb connection string."""
+    def get_name(self) -> str:
+        """Get name of worker."""
+        return f"{self.NAME}"
+
+    def broker_connection_str(self) -> str:
+        """Constructor for rabbitmq connection string."""
         return (
             "pyamqp://"
-            f"{self.USERNAME}:{self.PASSWORD.get_secret_value()}"
-            f"@{self.HOST}:{self.PORT}"
+            f"{self.BROKER_USERNAME}:{self.BROKER_PASSWORD.get_secret_value()}"
+            f"@{self.BROKER_HOST}:{self.BROKER_PORT}"
+        )
+
+    def result_connection_str(self) -> str:
+        """Constructor for redis connection string."""
+        return (
+            "redis://"
+            f":{self.RESULT_PASSWORD.get_secret_value()}"
+            f"@{self.RESULT_HOST}:{self.RESULT_PORT}/0"
         )
